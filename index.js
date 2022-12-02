d324d18ab764747d1e304135b346ec62cbe0e991
tamirulalan <tamirulalan@gmail.com>
2022-12-01 19:40:59 -0500

    Finilizing the functionality

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express =require('express')
 
 let app =express()
+//sending a response for client when they enter 0 number of bottles
 app.get('/0', (req,res)=>{
     res.send('<h1> Sorry!! There is no more drink left. Start over again </h1>')
 })
+//sending a response when the input is in between 1 and 99
 app.get('/:numberOfBottles', (req,res)=>{
     if(req.params.numberOfBottles >=2 && req.params.numberOfBottles<=99){
          res.send(req.params.numberOfBottles + " Bottles of beer on the wall, <a href = '" + (req.params.numberOfBottles-1)+ "'>take one down, pass it around</a>")
@@ -13,5 +15,5 @@ app.get('/:numberOfBottles', (req,res)=>{
    
 })
 app.listen(3000,()=>{
-console.log('Started listening to port 3000')
+console.log('Started listening 👂 to port 3000') 
 })
